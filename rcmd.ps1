a8295a039f552349e8413c61284103eb78813539
Victor Ordu <victorordu@outlook.com>
2023-08-22 13:56:59 +0100

    Add param for building the website

--- rcmd.ps1
+++ rcmd.ps1
@@ -5,10 +5,11 @@
 [CmdletBinding()]
 
 PARAM ( 
-    [string]$Pkgname = "naijR",
-    [switch]$Build,
+    [string]$PackageName = "naijR",
+    [switch]$BuildPackage,
     [switch]$Check,
-    [switch]$Test
+    [switch]$Test,
+    [switch]$BuildSite
 )
 
 # === Internal functions =====================
@@ -16,55 +17,63 @@ PARAM (
 function Get-PkgDescriptor ([string]$Key) {
     $descrfile = "DESCRIPTION"
     Write-Host "Checking $descrfile key '$Key'" -ForegroundColor DarkYellow
-    $descrfile = Join-Path $pkgname -ChildPath $descrfile | Resolve-Path
-    $line = Get-Content $descrfile | Where-Object { $_.StartsWith($Key)}
+    $descrfile = Join-Path $PackageName -ChildPath $descrfile | `
+                    Resolve-Path
+    $line = Get-Content $descrfile | `
+                Where-Object { $_.StartsWith($Key)}
     $line.Split(": ")[1]
     # & "Rscript" -e "as.symbol(unname(read.dcf('$descrfile')[, '$Key']))"
 }
 
 
 # Builds a source package
-function buildPackage {
+function buildSourcePackage {
     Write-Host "Building the package" -ForegroundColor DarkYellow
     $opts = $null
-    
-    if ($pkgname -eq "naijR") {
-        $opts = "--resave-data"
-        Write-Host "Using option(s) '$opts'" -ForegroundColor DarkYellow
-    }
-    & "Rcmd.exe" build $opts $pkgname
+    & "Rcmd.exe" build $opts $PackageName
 }
 
 
 # === Main program flow ================================
 $source_version = Get-PkgDescriptor -Key "Version"
-Write-Host "$pkgname : Package version is $source_version" -ForegroundColor DarkYellow
+Write-Host "$PackageName : Package version is $source_version" -ForegroundColor DarkYellow
 
 if ($Test) {
-    Rscript.exe -e "devtools::test('$pkgname')"
+    Rscript.exe -e "devtools::test('$PackageName')"
 }
 
-if ($Build) {
-    buildPackage
+if ($BuildPackage) {
+    buildSourcePackage
 }
 
 if ($Check) {
     $tarext = ".tar.gz"
     $wildcard = "_*.9*" + $tarext
-    $pattern = $pkgname + $wildcard
+    $pattern = $PackageName + $wildcard
 
     $latest_build = Get-ChildItem -Filter $pattern | `
         Select-Object -Property Name | `
         Sort-Object -Bottom 1
 
     $tarball = $latest_build.Name
-    $built_version = $tarball.Replace($pkgname + "_", "").Replace($tarext, "")
+    $built_version = $tarball.Replace($PackageName + "_", "").Replace($tarext, "")
 
     if ($built_version -ne $source_version) {
-        buildPackage
+        buildSourcePackage
         $tarball = $tarball.Replace($built_version, $source_version)
     }
 
     Write-Host "Checking the package" -ForegroundColor DarkYellow
     & "Rcmd.exe" check --as-cran --no-tests $tarball
+}
+
+if ($BuildSite) {
+    Write-Host "Building the package website" -ForegroundColor DarkYellow
+    $currBranch = git branch --show-current
+
+    if ($currBranch -ne "master") {
+        Write-Error "Git is not on the recommended 'master' branch for building the site"
+        throw "Git is currently on branch '$currBranch'"
+    }
+    Rscript.exe -e "pkgdown::build_site(pkg = '$PackageName', preview = FALSE, lazy = TRUE)"
 }
