5557cf3100e28be13c1101cc2ffff617d52c4105
Victor Ordu <victorordu@outlook.com>
2023-08-23 18:55:20 +0100

    Add param for 'goodpractice' review of package

--- rcmd.ps1
+++ rcmd.ps1
@@ -7,38 +7,42 @@
 PARAM ( 
     [string]$PackageName = "naijR",
     [ValidateSet("Source", "Binary", "Both")]
-    [string]$BuildPackage = "Source",
+    [string]$BuildPackage,
+    [ValidateSet("Basic", "Full")]
+    [string]$ReviewPackage,
     [switch]$Check,
     [switch]$Test,
     [switch]$BuildSite
 )
 
-# === Internal functions =====================
+# === Internals =====================
 # Default colouring for messaging
 $DefaultColour = "DarkYellow"
+
 # Retrieves the value for a given field in the package's DESCRIPTION file
-function Get-PkgDescriptor ([string]$Key) {
-    $descrfile = "DESCRIPTION"
-    Write-Host "Checking $descrfile key '$Key'" -ForegroundColor $DefaultColour
-    $descrfile = Join-Path $PackageName -ChildPath $descrfile | `
+$getPkgDescriptor = {
+    param([string]$Key)
+    $desc = Join-Path $PackageName -ChildPath "DESCRIPTION" | `
                     Resolve-Path
-    $line = Get-Content $descrfile | `
-                Where-Object { $_.StartsWith($Key)}
+    
+    if (-not (Test-Path $desc)) {
+        throw "'$PackageName' is not an R package"
+    }
+    $line = Get-Content $desc | Where-Object { $_.StartsWith($Key)}
     $line.Split(": ")[1]
-    # & "Rscript" -e "as.symbol(unname(read.dcf('$descrfile')[, '$Key']))"
 }
 
 
-# Builds a source package
-function buildSourcePackage {
-    Write-Host "Building the package" -ForegroundColor $DefaultColour
+# Script block for building a source package
+$buildSourcePackage = {
+    Write-Host "Building source package" -ForegroundColor $DefaultColour
     $opts = $null
     & "Rcmd.exe" build $opts $PackageName
 }
 
 
 # === Main program flow ================================
-$source_version = Get-PkgDescriptor -Key "Version"
+$source_version = & $getPkgDescriptor -Key "Version"
 Write-Host "$PackageName : Package version is $source_version" -ForegroundColor $DefaultColour
 
 if ($Test) {
@@ -46,18 +50,55 @@ if ($Test) {
 }
 
 if ($BuildPackage -eq "Source" -or $BuildPackage -eq "Both") {
-    buildSourcePackage
+    & $buildSourcePackage
 }
 
 if ($BuildPackage -eq "Binary" -or $BuildPackage -eq "Both") {
     Write-Host "Building binary package" -ForegroundColor $DefaultColour
+    $pvt = $PSVersionTable
+
+    if ($pvt.Platform -ne "Win32NT") {
+        throw "Binary build is only supported on Windows"
+    }
     & "Rcmd.exe" INSTALL --build $PackageName
+}
+
+if ($BuildPackage -eq "Both") {
+    $archDir = "arch"
+    $archPath = Resolve-Path $archDir
+
+    if (-not (Test-Path $archPath)) {
+        throw "Directory '$archPath' was not found"
+    }
+    Write-Host "Moving built source and binary archives to '$archPath'"
+    $archWildcard = $PackageName + "_" + $source_version + ".*"
+    Move-Item -Path $archWildcard-Destination $archPath -Force
+}
+
+if ($ReviewPackage -ne $null) {
+    Write-Host "Running automated review of the package" -ForegroundColor $DefaultColour
+    $rvpkg = $ReviewPackage.ToLower()
+    $gp = "goodpractice"
+    $repo = "https://cran.rstudio.com"
+    $dttm = Get-Date -Format "yyyy-MM-dd_HHmm"
+    $gpLogFile = $gp + "-" + $rvpkg + "_" + $dttm +  ".log"
+    $gpLogPath = Resolve-Path "logs"
+
+    if (-not (Test-Path $gpLogPath)) {
+        throw "The directory '$gpLogPath' was not found"
+    }
+    $gpFilepath = Join-Path -Path $gpLogPath -ChildPath $gpLogFile
+
+    Rscript -e "if (!requireNamespace('$gp')) install.packages('$gp', repos = '$repo'); library($gp)" `
+            -e "chks <- all_checks(); if ('$rvpkg' == 'basic') chks <- chks[!grepl('^rcmd', chks)]" `
+            -e "gp('$PackageName', checks = chks, quiet = FALSE)" | `
+                Out-File -FilePath $gpFilepath -Encoding "ascii"
 }
 
 if ($Check) {
     $tarext = ".tar.gz"
-    $wildcard = "_*.9*" + $tarext
-    $pattern = $PackageName + $wildcard
+    $tarWildcard = "_*.9*" + $tarext
+    $pattern = $PackageName + $tarWildcard
 
     $latest_build = Get-ChildItem -Filter $pattern | `
         Select-Object -Property Name | `
@@ -67,7 +108,7 @@ if ($Check) {
     $built_version = $tarball.Replace($PackageName + "_", "").Replace($tarext, "")
 
     if ($built_version -ne $source_version) {
-        buildSourcePackage
+        & $buildSourcePackage
         $tarball = $tarball.Replace($built_version, $source_version)
     }
 
